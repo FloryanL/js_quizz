1f5ae145299d1aefae129f5daaee3019269335d6
Floryan Lollivier <floryan.lollivier@gmail.com>
2016-07-25 01:11:19 +0200

    exo supprimer bis

--- script.js
+++ script.js
@@ -3,24 +3,6 @@
 
 //alert("Welcome !! Voici mon quizz !");
 
-
-//exo
-/*
-var meteo = "pluie";
-var meteo = "soleil";
-*/
-/*var meteo = "brumeux";
-
-if (meteo === "pluie") {
-  console.log("prends un parapluie");
-}
-else if (meteo === "brumeux"){
-  console.log("reste à la maison");
-}
-else {
-  console.log("mets toi en short");
-}*/
-
 var quiz;
 var question;
 var reponse;
