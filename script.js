47ae04c2d533c1cd9f6cb52882d597974f00a9ca
Floryan Lollivier <floryan.lollivier@gmail.com>
2016-07-25 00:21:23 +0200

    js tuto

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 /* JS */
 //JS
 
-alert("Welcome !! Voici mon quizz !");
+//alert("Welcome !! Voici mon quizz !");
+
+
+//exo
+/*var meteo = "brumeux";
+
+if (meteo === "pluie") {
+  console.log("prends un parapluie");
+}
+else if (meteo === "brumeux"){
+  console.log("reste à la maison");
+}
+else {
+  console.log("mets toi en short");
+}*/
+
+var quiz;
+var question;
+var reponse;
