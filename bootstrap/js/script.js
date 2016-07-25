2db23ab8fd9472e8ed927c34ed7ba4d282de8227
FloryanL <floryan.lollivier@gmail.com>
2016-07-25 16:04:21 +0200

    reconfiguration link script dans head

---
+++ bootstrap/js/script.js
@@ -0,0 +1,13 @@
+/* JS */
+//JS
+
+//alert("Welcome !! Voici mon quizz !");
+/*
+var quiz;
+var question;
+var reponse;*/
+
+//$(".buttonext").click();
+$("this").hide();
+
+console.log("hey");
--- script.js
+++
@@ -1,8 +0,0 @@
-/* JS */
-//JS
-
-//alert("Welcome !! Voici mon quizz !");
-
-var quiz;
-var question;
-var reponse;
