6a4330389a0a8c1a0cc06bf4f7b5a4ee57bc953d
Floryan Lollivier <floryan.lollivier@gmail.com>
2016-07-25 22:43:35 +0200

    ajout in script.js comm = help compréhension readme

--- bootstrap/js/script.js
+++ bootstrap/js/script.js
@@ -5,6 +5,7 @@
 //console.log("hey");
 
 //faire en sorte que mon bouton "next" affiche ma 2ème question mais aussi cache la 1ère
+//background-color : red; = mauvais réponse & background-color : green; = bonne réponse
 
 $(document).ready(function() {
     $("#Question").hide();
