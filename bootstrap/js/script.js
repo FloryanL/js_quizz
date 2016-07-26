6a983b244ad538c175fc9227ce10457267f79dbf
FloryanL <floryan.lollivier@gmail.com>
2016-07-26 17:03:38 +0200

    modif div des boutons prev & next

--- bootstrap/js/script.js
+++ bootstrap/js/script.js
@@ -10,60 +10,65 @@
 $(document).ready(function() {
     $("#Question2, #Question3, #Question4, #Question5, #Question6, #Question7, #Question8, #Question9, #Question10").hide();
 
+    //pourrait être optimisé je pense
+    
     $(".buttonext").on('click', function() {
-        $("#Question2").show(1000) && $("#Question1").hide();
+        $("#Question2").fadeIn(1000) && $("#Question1").hide();
     })
     $(".buttonprev").click( function() {
-        $("#Question1").show() && $("#Question2").hide();
+        $("#Question1").fadeIn() && $("#Question2").hide();
     })
-    $(".buttonext1").on('click', function() {
-        $("#Question3").show(1000) && $("#Question2").hide();
+    $(".buttonext").on('click', function() {
+        $("#Question3").fadeIn(1000) && $("#Question2").hide();
     })
-    $(".buttonprev1").click( function() {
-        $("#Question2").show() && $("#Question3").hide();
+    $(".buttonprev").click( function() {
+        $("#Question2").fadeIn() && $("#Question3").hide();
     })
-    $(".buttonext2").on('click', function() {
-        $("#Question4").show(1000) && $("#Question3").hide();
+    $(".buttonext").on('click', function() {
+        $("#Question4").fadeIn(1000) && $("#Question3").hide();
     })
-    $(".buttonprev2").click( function() {
-        $("#Question3").show() && $("#Question4").hide();
+    $(".buttonprev").click( function() {
+        $("#Question3").fadeIn() && $("#Question4").hide();
     })
-    $(".buttonext3").on('click', function() {
-        $("#Question5").show(1000) && $("#Question4").hide();
+    $(".buttonext").on('click', function() {
+        $("#Question5").fadeIn(1000) && $("#Question4").hide();
     })
-    $(".buttonprev3").click( function() {
-        $("#Question4").show() && $("#Question5").hide();
+    $(".buttonprev").click( function() {
+        $("#Question4").fadeIn() && $("#Question5").hide();
     })
-    $(".buttonext4").on('click', function() {
-        $("#Question6").show(1000) && $("#Question5").hide();
+    $(".buttonext").on('click', function() {
+        $("#Question6").fadeIn(1000) && $("#Question5").hide();
     })
-    $(".buttonprev4").click( function() {
-        $("#Question5").show() && $("#Question6").hide();
+    $(".buttonprev").click( function() {
+        $("#Question5").fadeIn() && $("#Question6").hide();
     })
-    $(".buttonext5").on('click', function() {
-        $("#Question7").show(1000) && $("#Question6").hide();
+    $(".buttonext").on('click', function() {
+        $("#Question7").fadeIn(1000) && $("#Question6").hide();
     })
-    $(".buttonprev5").click( function() {
-        $("#Question6").show() && $("#Question7").hide();
+    $(".buttonprev").click( function() {
+        $("#Question6").fadeIn() && $("#Question7").hide();
     })
-    $(".buttonext6").on('click', function() {
-        $("#Question8").show(1000) && $("#Question7").hide();
+    $(".buttonext").on('click', function() {
+        $("#Question8").fadeIn(1000) && $("#Question7").hide();
     })
-    $(".buttonprev6").click( function() {
-        $("#Question7").show() && $("#Question8").hide();
+    $(".buttonprev").click( function() {
+        $("#Question7").fadeIn() && $("#Question8").hide();
     })
-    $(".buttonext7").on('click', function() {
-        $("#Question9").show(1000) && $("#Question8").hide();
+    $(".buttonext").on('click', function() {
+        $("#Question9").fadeIn(1000) && $("#Question8").hide();
     })
-    $(".buttonprev7").click( function() {
-        $("#Question8").show() && $("#Question9").hide();
+    $(".buttonprev").click( function() {
+        $("#Question8").fadeIn() && $("#Question9").hide();
     })
-    $(".buttonext8").on('click', function() {
-        $("#Question10").show(1000) && $("#Question9").hide();
+    $(".buttonext").on('click', function() {
+        $("#Question10").fadeIn(1000) && $("#Question9").hide();
     })
-    $(".buttonprev8").click( function() {
-        $("#Question9").show() && $("#Question10").hide();
+    $(".buttonprev").click( function() {
+        $("#Question9").fadeIn() && $("#Question10").hide();
     })
+
+    //reponse correct
+    $("p.reponse").css(".reponsetrue");
 });
     
     
